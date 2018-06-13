d16f86674931063452c785e105308ebc865501da
Igor Solkin <inbox@tomclaw.com>
2018-06-13 22:24:22 +0300

    Update build tools and Kotlin versions

--- app/src/main/java/com/tomclaw/kvassword/MainActivity.kt
+++ app/src/main/java/com/tomclaw/kvassword/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
                 reader.close()
             } catch (ignored: IOException) {
             }
-
         }
         randomWord = RandomWord(grammar)
 
