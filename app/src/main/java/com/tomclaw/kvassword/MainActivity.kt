0e70958c88e9ae6a77a60776bdd42cc08895181f
Igor Solkin <inbox@tomclaw.com>
2018-06-14 23:25:56 +0300

    Add generated password strengths policies

--- app/src/main/java/com/tomclaw/kvassword/MainActivity.kt
+++ app/src/main/java/com/tomclaw/kvassword/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tomclaw.kvassword
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.RadioGroup
 import android.widget.TextView
 import com.google.gson.GsonBuilder
 import java.io.IOException
@@ -14,12 +15,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var random: Random
     private lateinit var randomWord: RandomWord
 
+    private var button: Button? = null
+    private var password: TextView? = null
+    private var strength: RadioGroup? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        val password = findViewById<TextView>(R.id.password)
+        button = findViewById(R.id.button)
+        password = findViewById(R.id.password)
+        strength = findViewById(R.id.pass_strength)
 
         random = Random(System.currentTimeMillis())
 
@@ -36,18 +42,37 @@ class MainActivity : AppCompatActivity() {
         }
         randomWord = RandomWord(grammar)
 
-        button.setOnClickListener {
-            generate(password)
+        button?.setOnClickListener {
+            generate()
         }
 
-        generate(password)
+        generate()
     }
 
-    private fun generate(text: TextView) {
-        val pass = randomWord.nextWord(3).toFirstUpper() +
-                randomDigit() + randomWord.nextWord(3).toFirstUpper() + randomSymbol() +
-                randomWord.nextWord(3).toUpperCase()
-        text.text = pass
+    private fun generate() {
+        val pass = when (strength?.checkedRadioButtonId) {
+            R.id.pass_good -> listOf(
+                    randomWord.nextWord(4).toFirstUpper(),
+                    randomDigit(),
+                    randomDigit()
+            )
+            R.id.pass_strong -> listOf(
+                    randomWord.nextWord(3).toFirstUpper(),
+                    randomDigit(),
+                    randomWord.nextWord(3).toFirstUpper(),
+                    randomSymbol()
+            )
+            R.id.pass_very_strong -> listOf(
+                    randomWord.nextWord(3).toFirstUpper(),
+                    randomDigit(),
+                    randomWord.nextWord(3).toFirstUpper(),
+                    randomSymbol(),
+                    randomDigit(),
+                    randomWord.nextWord(3).toUpperCase()
+            )
+            else -> throw IllegalStateException("Invalid selection")
+        }
+        password?.text = pass.concatItems()
     }
 
     private fun randomDigit(): String = random.nextInt(10).toString()
@@ -57,6 +82,12 @@ class MainActivity : AppCompatActivity() {
         val i = random.nextInt(symbols.length)
         return symbols.substring(i, i + 1)
     }
+}
+
+private fun List<String>.concatItems(): kotlin.String {
+    val string = StringBuilder()
+    forEach { string.append(it) }
+    return string.toString()
 }
 
 private fun String.toFirstUpper(): String {
