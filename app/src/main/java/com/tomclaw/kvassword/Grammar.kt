146a76b6b80b55641abd366d361a497b816f2788
Igor Solkin <inbox@tomclaw.com>
2018-10-19 22:16:17 +0300

    Update build configuration

--- app/src/main/java/com/tomclaw/kvassword/Grammar.kt
+++ app/src/main/java/com/tomclaw/kvassword/Grammar.kt
@@ -1,5 +1,7 @@
 package com.tomclaw.kvassword
 
-class Grammar(val startBiGram: Array<String>,
-              val lookupBiGram: Array<String>,
-              val nextCharLookup: Array<Array<Array<String>>>) : Unobfuscatable
+class Grammar(
+        val startBiGram: Array<String>,
+        val lookupBiGram: Array<String>,
+        val nextCharLookup: Array<Array<Array<String>>>
+) : Unobfuscatable
